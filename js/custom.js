8010c9de98f7907a37e0f2162c1889f2f76a58c0
MaximeWeisser <maxime.weisser@gmail.com>
2017-04-06 15:23:46 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,4 @@
-function fonctionR(url, elementID){
+/*function fonctionR(url, elementID){
 var xmlhttp = new XMLHttpRequest();
 xmlhttp.onreadystatechange = function() {
 if (this.readyState == 4 && this.status == 200) {
@@ -30,4 +30,32 @@ function homePage(){
 	elementID = 'demo3';
 	fonctionR(url, elementID);
 
-}
+}*/
+
+$(document).ready(function() {
+	$.ajax({
+		url : "http://bitcoin.mubiz.com/info",
+		dataType : "json",
+		contentType : "application/json; charset=utf-8",
+		type : "GET",
+		timeout:	"5000",
+		async : false,
+
+		success : function(data) {
+			$('#bitcoin_block_number').append(data.blocks);
+			$('#bitcoin_difficulty').append(data.difficulty);
+			$('#bitcoin_version').append(data.version);
+			$('#bitcoin_protocolversion').append(data.protocolversion);
+			$('#bitcoin_connections').append(data.connections);
+			$('#bitcoin_timeoffset').append(data.timeoffset);
+		},
+		error : function(xhr, status, err) {
+			$('#bitcoin_block_number').append(err+" N/A");
+			$('#bitcoin_difficulty').append(err+" N/A");
+			$('#bitcoin_version').append(err+" N/A");
+			$('#bitcoin_protocolversion').append(err+" N/A");
+			$('#bitcoin_connections').append(err+" N/A");
+			$('#bitcoin_timeoffset').append(err+" N/A");
+		}
+	});
+});
