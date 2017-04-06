6ca4d00a6af7b8bd83407afb0dc7bb1986107a5f
MaximeWeisser <maxime.weisser@gmail.com>
2017-04-06 16:42:40 +0200

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -34,7 +34,7 @@ function homePage(){
 
 $(document).ready(function() {
 	$.ajax({
-		url : "https://blockchain.info/fr/rawtx/$tx_index",
+		url : "https://mubiz.com/@user_url/bitcoin/montant/référence/emetteur/",
 		dataType : "json",
 		contentType : "application/json; charset=utf-8",
 		type : "GET",
